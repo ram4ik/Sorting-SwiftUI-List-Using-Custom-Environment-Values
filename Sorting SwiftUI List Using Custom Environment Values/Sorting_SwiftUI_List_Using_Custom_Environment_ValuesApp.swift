f65c4dad30d68d6af266cf91dc81a148f40da211
Ramill <ram4ik@hotmail.com>
2024-10-28 19:21:42 +0200

    Initial Commit

---
+++ Sorting SwiftUI List Using Custom Environment Values/Sorting_SwiftUI_List_Using_Custom_Environment_ValuesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sorting_SwiftUI_List_Using_Custom_Environment_ValuesApp.swift
+//  Sorting SwiftUI List Using Custom Environment Values
+//
+//  Created by admin on 10/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Sorting_SwiftUI_List_Using_Custom_Environment_ValuesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
